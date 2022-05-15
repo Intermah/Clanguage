a62eb96a564b93034a6b5a308c2be124f96658de
Intermah <89256569+Intermah@users.noreply.github.com>
2022-05-15 22:19:52 +0300

    Add files via upload

---
+++ test12_04.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+//Function doubles the number 
+int doubleNumber(int a);
+
+int main(){
+    
+    int a = 0;
+    std:: cout << doubleNumber(a) << std:: endl;
+
+return 0;
+}
+
+int doubleNumber(int a){
+return a*2;
+}
